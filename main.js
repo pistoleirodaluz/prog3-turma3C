93d54e5aa6dd936c547f3002b9f10a1ebdb33087
pistoleirodaluz <129514045+pistoleirodaluz@users.noreply.github.com>
2024-10-15 09:34:47 -0300

    sffdd

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    valorEntropia.textContent = "o gusavo te protegera por " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias .";
 }
 
 
